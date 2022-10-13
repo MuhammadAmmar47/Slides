f0c94086f05ad296a684a13440d9a86a42bf2355
MuhammadAmmar47 <115443134+MuhammadAmmar47@users.noreply.github.com>
2022-10-13 15:52:23 +0500

    Add files via upload

---
+++ Slides/Lecture 01/Functions, Array, List/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "";
+            string lastName = "";
+            string localeFormat = "";
+            string formatedName = "";
+            string[] formatedNames = new string[2];
+            List<string> lstFormatedNames = new List<string>();
+
+            firstName = "Ali";
+            lastName = "Ahmad";
+            localeFormat = "FL";
+
+            formatedName = FormatName(firstName, lastName, localeFormat);
+            formatedNames[0] = formatedName;
+            lstFormatedNames.Add(formatedName);
+            //Console.WriteLine(formatedName);
+
+
+            firstName = "Alishba";
+            lastName = "Tahir";
+            localeFormat = "LF";
+
+            formatedName = FormatName(firstName, lastName, localeFormat);
+            formatedNames[1] = formatedName;
+            lstFormatedNames.Add(formatedName);
+            //Console.WriteLine(formatedName);
+
+            for (int i = 0; i < formatedNames.Length; i++)
+            {
+                Console.WriteLine(formatedNames[i]);
+            }
+            for (int i = 0; i < lstFormatedNames.Count; i++)
+            {
+                Console.WriteLine("Your Name is:  " + lstFormatedNames[i]);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static string FormatName(string firstName, string lastName, string localeFormat)
+        {
+            string formatedName = "";
+            if (localeFormat == "FL")
+            {
+                formatedName = firstName + " " + lastName;
+            }
+            else
+            {
+                formatedName = lastName + " " + firstName;
+            }
+            return formatedName;
+        }
+    }
+}
